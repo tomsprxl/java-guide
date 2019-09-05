37178309da890cefeb1b267dde614b215d916e43
xuliang <18656756681@163.com>
2019-09-05 21:39:39 +0800

    add

---
+++ src/main/java/com/liangxu/day20190904/javase001.java
@@ -0,0 +1,9 @@
+package com.liangxu.day20190904;
+
+public class javase001 {
+
+    public javase001(){
+
+    }
+    
+}
---
+++ src/main/java/com/liangxu/day20190905/CollectionDemos.java
@@ -0,0 +1,52 @@
+package com.liangxu.day20190905;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CollectionDemos {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(-1);
+        arrayList.add(3);
+        arrayList.add(3);
+        arrayList.add(-5);
+        arrayList.add(7);
+        arrayList.add(4);
+        arrayList.add(-9);
+        arrayList.add(-7);
+        System.out.println("原始数组:");
+        System.out.println(arrayList);
+        // void reverse(List list)：反转
+        Collections.reverse(arrayList);
+        System.out.println("Collections.reverse(arrayList):");
+        System.out.println(arrayList);
+
+
+        Collections.rotate(arrayList, 9);
+        System.out.println("Collections.rotate(arrayList, 4):");
+        System.out.println(arrayList);
+
+        // void sort(List list),按自然排序的升序排序
+        Collections.sort(arrayList);
+        System.out.println("Collections.sort(arrayList):");
+        System.out.println(arrayList);
+
+        // void shuffle(List list),随机排序
+        Collections.shuffle(arrayList);
+        System.out.println("Collections.shuffle(arrayList):");
+        System.out.println(arrayList);
+
+        // void swap(List list, int i , int j),交换两个索引位置的元素
+        Collections.swap(arrayList, 2, 5);
+        System.out.println("Collections.swap(arrayList, 2, 5):");
+        System.out.println(arrayList);
+
+        // 定制排序的用法
+        Collections.sort(arrayList, Integer::compareTo);
+
+        System.out.println("定制排序后：");
+        System.out.println(arrayList);
+    }
+
+}
